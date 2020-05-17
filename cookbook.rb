4ca6eb4324e11becd269c27e3a65558e1eae66e2
Bianca Bech <biancapaulabech@gmail.com>
2020-05-16 22:29:25 -0300

    Adicionando métodos

--- cookbook.rb
+++ cookbook.rb
@@ -1,45 +1,51 @@
-puts "Bem-vindo ao Cookbook, sua rede social de receitas"
+def bem_vindo
+  puts "Bem-vindo ao Cookbook, sua rede social de receitas"
+end
 
-receitas = []
-
-# puts "[1] Cadastrar uma receita"
-# puts "[2] Ver todas as receitas"
-# puts "[3] Sair"
-
-# print "Escolha uma opção: "
-# opcao = gets.to_i
-
-# puts "Digite o nome da receita:" if opcao != 3
-
-opcao = nil
-while(opcao != 3) do
+def menu
   puts "[1] Cadastrar uma receita"
   puts "[2] Ver todas as receitas"
   puts "[3] Sair"
 
   print "Escolha uma opção: "
-  opcao = gets.to_i
+  return gets.to_i()
+end
 
+def inserir_receita()
+  puts "Digite o nome da receita:"
+  nome = gets.chomp
+  puts "Digite o tipo da receita:"
+  tipo = gets.chomp
+  puts
+  puts "Receita de #{nome} cadastrada com sucesso!"
+  puts
+  return {nome: nome, tipo: tipo}
+end
+
+def imprimir_receitas(receitas)
+  puts "======= Receitas Cadastradas ======="
+  receitas.each do |receita|
+    puts "#{receita[:nome]} - #{receita[:tipo]}"
+  end
+  puts
+end
+
+bem_vindo()
+
+receitas = []
+
+opcao = menu()
+
+while(opcao != 3) do
   if(opcao == 1)
-    puts "Digite o nome da receita:"
-    nome = gets.chomp
-    puts "Digite o tipo da receita:"
-    tipo = gets.chomp
-
-    receitas << {nome: nome, tipo: tipo}
-    puts
-    puts "Receita de #{nome} cadastrada com sucesso!"
-    puts
+    receitas << inserir_receita() 
   elsif(opcao == 2)
-    puts "======= Receitas Cadastradas ======="
-    receitas.each do |receita|
-      puts "#{receita[:nome]} - #{receita[:tipo]}"
-    end
-    puts "======= FIM ======="
-    puts
-  elsif (opcao != 3)
+    imprimir_receitas(receitas)
+  else (opcao != 3)
     puts "Opção Inválida!"
   end
+  
+  opcao = menu()
 end
 
 puts "Obrigado por usar o Cookbook, até logo!"
